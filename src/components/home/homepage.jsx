83097d4e21c633ef0cd5db505a852c6ce792f69a
João Vitor Alves <joaovtoralves@gmail.com>
2024-10-10 12:43:04 -0300

    change cards to velzon example

--- src/components/home/homepage.jsx
+++ src/components/home/homepage.jsx
@@ -7,47 +7,8 @@ const HomePage = () => {
   return (
     <div className="containerHome">
       <Navbar />
-      <h1>CARDS TITULO TESTE</h1>
+      <h1>CRM</h1>
       <div className="cards">
-        <div className="cardstyle">
-          <img src={cardteste} alt="" />
-          <h2>Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
-            deserunt saepe, repellat autem quae
-          </p>
-          <Link to={"#"}>
-            {" "}
-            <a>Saiba Mais</a>{" "}
-          </Link>
-        </div>
-
-        <div className="cardstyle">
-          <img src={cardteste} alt="" />
-          <h2>Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
-            deserunt saepe, repellat autem quae
-          </p>
-          <Link to={"#"}>
-            {" "}
-            <a>Saiba Mais</a>{" "}
-          </Link>
-        </div>
-
-        <div className="cardstyle">
-          <img src={cardteste} alt="" />
-          <h2>Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
-            deserunt saepe, repellat autem quae
-          </p>
-          <Link to={"#"}>
-            {" "}
-            <a>Saiba Mais</a>{" "}
-          </Link>
-        </div>
-
         <div className="cardstyle">
           <img src={cardteste} alt="" />
           <h2>Title</h2>
