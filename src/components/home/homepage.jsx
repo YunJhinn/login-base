d9bebfb5d77267ce302145d56ddf3f376187dc1f
João Vitor Alves <joaovtoralves@gmail.com>
2024-10-10 12:53:45 -0300

    change card to profit example

--- src/components/home/homepage.jsx
+++ src/components/home/homepage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../navbar";
 import { Link } from "react-router-dom";
 import cardteste from "./cardteste.avif";
 import "./homepage.css";
+import { GiProfit } from "react-icons/gi";
 
 const HomePage = () => {
   return (
@@ -10,16 +11,12 @@ const HomePage = () => {
       <h1>CRM</h1>
       <div className="cards">
         <div className="cardstyle">
-          <img src={cardteste} alt="" />
-          <h2>Title</h2>
+          <h2>Anual Profit</h2>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
-            deserunt saepe, repellat autem quae
+            <GiProfit className="icon-card" />
+            1987.23
           </p>
-          <Link to={"#"}>
-            {" "}
-            <a>Saiba Mais</a>{" "}
-          </Link>
         </div>
 
         <div className="cardstyle">
