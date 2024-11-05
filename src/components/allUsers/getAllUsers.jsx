2333494efab842f79c341baacd00fae2fcedebe5
João Vitor Alves <joaovtoralves@gmail.com>
2024-11-05 08:15:53 -0300

    add responsivity to allusers,footer,homepage and tableISC

--- src/components/allUsers/getAllUsers.jsx
+++ src/components/allUsers/getAllUsers.jsx
@@ -8,6 +8,7 @@ const GetAllUsers = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   let config = {
     headers: {
       Authorization: "Bearer " + localStorage.getItem("token"),
@@ -18,7 +19,6 @@ const GetAllUsers = () => {
     async function fetchUsers() {
       try {
         const res = await api.get("/api/get_users", config);
-
         setUsers(res.data);
         console.log(res.data);
       } catch (error) {
@@ -31,21 +31,30 @@ const GetAllUsers = () => {
 
     fetchUsers();
   }, []);
+
   return (
     <div className="container-users">
       <Navbar />
-
-      {users.map((user) => (
-        <div className="card-usuarios" key={user._id?.$oid}>
-          <img src="https://www.gravatar.com/avatar/HASH" alt="imagem teste" />
-          <h2>Nome: {user.fullname || "N/A"}</h2>
-          <p>
-            Cargo:{user.cargo || "N/A"} / credencial:
-            {user.credencial || "N/A"}
-          </p>
-          <h3>Empresa: {user.empresa_nome || "N/A"}</h3>
-        </div>
-      ))}
+      {loading ? (
+        <p>Carregando...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        users.map((user) => (
+          <div className="card-usuarios" key={user._id?.$oid}>
+            <img
+              src="https://www.gravatar.com/avatar/HASH"
+              alt="imagem teste"
+            />
+            <h2>Nome: {user.fullname || "N/A"}</h2>
+            <p>
+              Cargo: {user.cargo || "N/A"} / Credencial:{" "}
+              {user.credencial || "N/A"}
+            </p>
+            <h3>Empresa: {user.empresa_nome || "N/A"}</h3>
+          </div>
+        ))
+      )}
       <Footer />
     </div>
   );
