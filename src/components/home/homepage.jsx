526ec88faa06f57fe4b34fb0676bdff6a1e4aabd
João Vitor Alves <joaovtoralves@gmail.com>
2024-11-06 11:22:46 -0300

    add my team on home page and responsivity

--- src/components/home/homepage.jsx
+++ src/components/home/homepage.jsx
@@ -1,21 +1,10 @@
 import Navbar from "../navbar/navbar";
 import { Link } from "react-router-dom";
-import cardteste from "./cardteste.avif";
-import "./homepage.css";
-import { GiProfit } from "react-icons/gi";
-import { MdLeaderboard } from "react-icons/md";
-import { FaDailymotion } from "react-icons/fa";
-import { FaIdeal } from "react-icons/fa";
-import { FaNoteSticky } from "react-icons/fa6";
-import Bar from "../CHARTS/Chart.jsx";
-import DonutCharts from "../CHARTS/DonutCharts.jsx";
 
-import Line from "../CHARTS/LineChart.jsx";
-import Area from "../CHARTS/AreaChart.jsx";
+import "./homepage.css";
+
 import Footer from "../footer/footer";
 
-import ChartBar from "../CHARTS/ChartBar.jsx";
-import SyncronizeCharts from "../CHARTS/SyncronizeCharts.jsx";
 import Carroussel from "../carroussel/Carroussel.jsx";
 import PoliticaIntegrada from "../poli-integrada/PoliticaIntegrada.jsx";
 import UsersHome from "../usersHome/UsersHome.jsx";
@@ -34,7 +23,7 @@ const HomePage = () => {
       <div className="politica">
         <PoliticaIntegrada />
       </div>
-      <h2>Nossa Equipe</h2>
+      <h2 className="equipe">Nossa Equipe</h2>
       <div className="users-home">
         <UsersHome />
       </div>
