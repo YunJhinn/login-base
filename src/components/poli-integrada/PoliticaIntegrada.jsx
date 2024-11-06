1139b6ad48cc9c2a8d003829c694ba6e546e039e
João Vitor Alves <joaovtoralves@gmail.com>
2024-11-06 18:30:52 -0300

    add icons in integrated policy

--- src/components/poli-integrada/PoliticaIntegrada.jsx
+++ src/components/poli-integrada/PoliticaIntegrada.jsx
@@ -1,39 +1,46 @@
 import React from "react";
 import "./PoliticaIntegrada.css";
+import { FcStatistics } from "react-icons/fc";
+import { MdPolicy } from "react-icons/md";
+import { CgDanger } from "react-icons/cg";
+import { MdHealthAndSafety } from "react-icons/md";
+import { RiTeamFill } from "react-icons/ri";
+import { MdHighQuality } from "react-icons/md";
+import { PiTreeBold } from "react-icons/pi";
 
 const PoliticaIntegrada = () => {
   return (
     <div className="container-politica">
       <div className="card-politica">
         <div className="card-politica-style-1">
-          icon
+        <FcStatistics />
           <h2>Melhoria Contínua do SGI</h2>
         </div>
         <div className="card-politica-style-2">
-          icon
+        <MdPolicy />
           <h2>Cumprimento dos requisitos legais e outros pertinentes</h2>
         </div>
         <div className="card-politica-style-3">
-          icon
+        <CgDanger />
           <h2>Eliminar perigos e reduzir riscos</h2>
         </div>
         <div className="card-politica-style-4">
-          icon
+        <MdHealthAndSafety />
           <h2>
             Proporcionar condições seguras e saudáveis, prevenindo lesóes e
             problemas de saúde
           </h2>
         </div>
         <div className="card-politica-style-5">
-          icon
+        <RiTeamFill />
           <h2>Consulta e participação dos trabalhadores</h2>
         </div>
         <div className="card-politica-style-6">
-          icon
+        <MdHighQuality />
           <h2>Qualidade dos serviçoes realizados</h2>
         </div>
         <div className="card-politica-style-7">
-          icon
+        <PiTreeBold />
           <h2>Prevenção da poluição e proteção do meio ambiente</h2>
         </div>
       </div>
